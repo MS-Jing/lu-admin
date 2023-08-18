ff49e10a0bec4d8b74b899f18d7d7cb0d2fe5b8e
luojing <1126184155@qq.com>
2023-08-18 18:48:39 +0800

    代码结构调整

--- utils/src/main/java/com/lj/utils/query/condition/AbstractConditionHandler.java
+++ utils/src/main/java/com/lj/utils/query/condition/AbstractConditionHandler.java
@@ -2,12 +2,9 @@ package com.lj.utils.query.condition;
 
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.ObjectUtil;
-import cn.hutool.core.util.ReflectUtil;
 import cn.hutool.core.util.StrUtil;
 import com.lj.utils.query.AbstractQueryParams;
-import com.lj.utils.query.ParamsFieldDetail;
-import com.lj.utils.query.QueryWrapper;
-import com.lj.utils.query.WrapperFun;
+import com.lj.utils.query.details.ConditionDetails;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
@@ -16,48 +13,18 @@ import java.util.Map;
 
 /**
  * @author luojing
- * @date 2023/8/11
- * 抽象的条件注解处理器
+ * @date 2023/8/18
+ * 所有的条件处理器都需要继承该类
  */
-public abstract class AbstractConditionHandler<A extends Annotation> {
+public abstract class AbstractConditionHandler<A extends Annotation> implements ConditionHandler<A> {
 
-    public ParamsFieldDetail<A> handleParamsField(Field paramField, A condition) {
-        return new ParamsFieldDetail<>(paramField, this, condition);
+    @Override
+    public ConditionDetails<A> getConditionDetails(Class<? extends AbstractQueryParams> paramsClass, Field paramField, Annotation conditionAnnotation) {
+        return handlerConditionDetails(paramsClass, paramField, (A) conditionAnnotation);
     }
 
-    public <T> void handleCondition(QueryWrapper<T> queryWrapper, ParamsFieldDetail<A> fieldDetails, AbstractQueryParams queryParams) {
-        Object fieldValue = ReflectUtil.getFieldValue(queryParams, fieldDetails.getParamField());
-        WrapperFun wrapperFun = getFun(queryWrapper, fieldDetails, fieldValue);
-        if (wrapperFun != null) {
-            // 这是在判断queryWrapper的查询条件是否需要对字段值进行判空
-            boolean isNotNull = !valueNotNull(fieldDetails) || this.isNotNull(fieldValue);
-            wrapperFun.condition(isNotNull, getColumn(fieldDetails), fieldValue);
-        } else {
-            handleCondition(queryWrapper, fieldDetails, fieldValue);
-        }
-    }
+    public abstract ConditionDetails<A> handlerConditionDetails(Class<? extends AbstractQueryParams> paramsClass, Field paramField, A conditionAnnotation);
 
-    /**
-     * 判断是否需要判断值为空
-     */
-    protected boolean valueNotNull(ParamsFieldDetail<A> fieldDetails) {
-        return true;
-    }
-
-    /**
-     * 获取数据库真实字段名称
-     */
-    protected String getColumn(ParamsFieldDetail<A> fieldDetails) {
-        return getColumn(fieldDetails.getParamField().getName(), "");
-    }
-
-    protected <T> void handleCondition(QueryWrapper<T> queryWrapper, ParamsFieldDetail<A> fieldDetails, Object fieldValue) {
-        throw new RuntimeException("方法不被支持 请确保实现handleCondition和getFun其一");
-    }
-
-    protected <T> WrapperFun getFun(QueryWrapper<T> queryWrapper, ParamsFieldDetail<A> fieldDetails, Object fieldValue) {
-        return null;
-    }
 
     protected boolean isNotNull(Object fieldValue) {
         if (fieldValue instanceof String) {
@@ -74,16 +41,7 @@ public abstract class AbstractConditionHandler<A extends Annotation> {
         return ObjectUtil.isNotNull(fieldValue);
     }
 
-    /**
-     * 数据库列名
-     * 因为列名可能会出现在注解上，如果没有那么使用字段名称，但是需要驼峰转下划线
-     *
-     * @param column       注解上的列名
-     * @param defaultValue 字段的名称
-     * @return 数据库列名
-     */
-    protected String getColumn(String column, String defaultValue) {
-        return StrUtil.toUnderlineCase(StrUtil.isNotBlank(column) ? column : defaultValue);
+    protected String transformColumn(String column) {
+        return StrUtil.toUnderlineCase(column);
     }
-
 }
