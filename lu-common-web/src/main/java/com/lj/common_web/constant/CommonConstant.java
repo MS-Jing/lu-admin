d6fff9a6ddab19e391b65e527af57c0404c4c99d
luojing <1126184155@qq.com>
2024-07-26 15:53:58 +0800

    返回string无法包装的问题

---
+++ lu-common-web/src/main/java/com/lj/common_web/constant/CommonConstant.java
@@ -0,0 +1,17 @@
+package com.lj.common_web.constant;
+
+/**
+ * @author luojing
+ * @version 1.0
+ * @date 2023/3/21 11:11
+ * 公共常量
+ */
+public interface CommonConstant {
+
+    /**
+     * 是否进行包装成ResponseVO的请求头
+     */
+    String RESPONSE_WRAP_HEADER = "response-wrap";
+
+
+}
--- lu-common-web/src/main/java/com/lj/common_web/response/ResponseControllerAdvice.java
+++ lu-common-web/src/main/java/com/lj/common_web/response/ResponseControllerAdvice.java
@@ -1,7 +1,10 @@
 package com.lj.common_web.response;
 
+import cn.hutool.core.util.TypeUtil;
 import cn.hutool.json.JSONUtil;
 import com.lj.common_web.annotation.ResponseResultVo;
+import com.lj.common_web.constant.CommonConstant;
+import com.lj.common_web.utils.ServletUtil;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -9,6 +12,8 @@ import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import javax.servlet.http.HttpServletResponse;
 
 
 /**
@@ -31,14 +36,21 @@ public class ResponseControllerAdvice implements ResponseBodyAdvice<Object> {
 
     @Override
     public Object beforeBodyWrite(Object data, MethodParameter returnType, MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest request, ServerHttpResponse response) {
+        // 如果本身就是ResponseVO那么直接返回
+        if (TypeUtil.getClass(returnType.getGenericParameterType()).equals(ResponseVO.class)) {
+            return data;
+        }
+        // 将原本的数据包装在ResultVO里
         ResponseVO<Object> responseVO = ResponseVO.success(data);
+        // 在请求头中提示这是被包装了的响应
+        HttpServletResponse httpServletResponse = ServletUtil.getResponse();
+        httpServletResponse.setHeader(CommonConstant.RESPONSE_WRAP_HEADER, CommonConstant.RESPONSE_WRAP_HEADER);
         // String类型不能直接返回，要进行些特别的处理
         if (returnType.getGenericParameterType().equals(String.class)) {
             response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
             // 将数据包装在ResultVO里后，再转换为json字符串响应给前端
             return JSONUtil.toJsonStr(responseVO);
         }
-        // 将原本的数据包装在ResultVO里
         return responseVO;
     }
 }
