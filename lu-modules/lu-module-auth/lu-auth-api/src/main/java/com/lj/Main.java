176fd5b39450d28c08eff2ece608cda21326eff6
luojing <1126184155@qq.com>
2024-08-05 18:40:26 +0800

    提供系统模块和任务模块解构

---
+++ lu-modules/lu-module-auth/lu-auth-api/src/main/java/com/lj/Main.java
@@ -0,0 +1,11 @@
+package com.lj;
+
+/**
+ * @author luojing
+ * @since 2024/8/5 18:22
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
