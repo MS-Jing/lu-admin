a3b01e5fc07dd5ebab77ccd6cc76f855e29a6f31
LuoJing <1126184155@qq.com>
2024-10-26 21:47:33 +0800

    app添加swagger配置

--- lu-app/src/main/java/com/lj/app/AppApplication.java
+++ lu-app/src/main/java/com/lj/app/AppApplication.java
@@ -1,7 +1,18 @@
 package com.lj.app;
 
+import cn.hutool.core.util.ClassUtil;
+import com.github.xiaoymin.knife4j.spring.extension.OpenApiExtensionResolver;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import javax.annotation.Resource;
 
 /**
  * @author luojing
@@ -11,6 +22,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
+    }
+
+    @Resource
+    private OpenApiExtensionResolver openApiExtensionResolver;
+
+
+    @Bean(value = "appApi")
+    public Docket appApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder()
+                        .title("app")
+                        .description("app")
+                        .version("1.0.0")
+                        .build())
+                .groupName("app")
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .apis(RequestHandlerSelectors.basePackage(ClassUtil.getPackage(AppApplication.class) + ".controller"))
+                .paths(PathSelectors.any())
+                .build().extensions(openApiExtensionResolver.buildExtensions("app"));
     }
 
 }
