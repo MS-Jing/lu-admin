de6fd2d9b45eeb76296fc9bfe57e4508d17e307c
LuoJing <1126184155@qq.com>
2024-11-24 14:34:37 +0800

    fix: 多环境，代码生成器只有在dev环境才会生效

--- lu-modules/lu-generator/src/main/java/com/lj/generator/GeneratorAutoConfiguration.java
+++ lu-modules/lu-generator/src/main/java/com/lj/generator/GeneratorAutoConfiguration.java
@@ -2,9 +2,11 @@ package com.lj.generator;
 
 import com.lj.common.module.ModuleInfo;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * @author luojing
@@ -13,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan("com.lj.generator")
 @MapperScan("com.lj.generator.mapper")
+@Profile("dev")
 public class GeneratorAutoConfiguration {
 
     @Bean
