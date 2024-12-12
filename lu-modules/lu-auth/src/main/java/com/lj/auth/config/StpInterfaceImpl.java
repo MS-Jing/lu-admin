b798ca315bd023b83a0020887a881686d6a4dafa
luojing <1126184155@qq.com>
2024-12-12 11:07:30 +0800

    feat: 用户权限，只有首次访问才会查询用户的权限列表

--- lu-modules/lu-auth/src/main/java/com/lj/auth/config/StpInterfaceImpl.java
+++ lu-modules/lu-auth/src/main/java/com/lj/auth/config/StpInterfaceImpl.java
@@ -1,13 +1,19 @@
 package com.lj.auth.config;
 
+import cn.dev33.satoken.session.SaSession;
 import cn.dev33.satoken.stp.StpInterface;
+import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.core.util.NumberUtil;
 import cn.hutool.core.util.StrUtil;
 import com.lj.sys.constant.SysConstant;
+import com.lj.sys.service.SysMenuService;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author luojing
@@ -17,6 +23,9 @@ import java.util.List;
 @Component
 public class StpInterfaceImpl implements StpInterface {
 
+    @Resource
+    private SysMenuService sysMenuService;
+
     @Override
     public List<String> getPermissionList(Object loginId, String loginType) {
         Long userId = getUserId(loginId);
@@ -24,7 +33,17 @@ public class StpInterfaceImpl implements StpInterface {
             // 超管拥有所有权限
             return Collections.singletonList("*");
         }
-        return Collections.emptyList();
+        List<String> permissionList = (List<String>) StpUtil.getTokenSession().get(SaSession.PERMISSION_LIST);
+        if (permissionList == null) {
+            permissionList = new ArrayList<>();
+            // 将所有的权限加入
+            Map<String, List<String>> buttonPermission = sysMenuService.buttonPermission(userId);
+            for (List<String> value : buttonPermission.values()) {
+                permissionList.addAll(value);
+            }
+            StpUtil.getTokenSession().set(SaSession.PERMISSION_LIST, permissionList);
+        }
+        return permissionList;
     }
 
     @Override
